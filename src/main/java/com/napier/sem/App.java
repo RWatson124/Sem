72ec2b6238d0b6725c3bca429d94f78ca121403c
RWatson124 <40681288@live.napier.ac.uk>
2024-09-27 16:35:07 +0100

    new attempt app.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,26 +5,31 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "Software Engineering Methods")
-                .append("year", "2021")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
-        collection.insertOne(doc);
+        try (MongoClient mongoClient = new MongoClient("localhost", 27000)) {
+            // Get a database - will create when we use it
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            // Get a collection from the database
+            MongoCollection<Document> collection = database.getCollection("test");
 
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+            // Create a document to store
+            Document doc = new Document("name", "Kevin Sim")
+                    .append("class", "Software Engineering Methods")
+                    .append("year", "2021")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+
+            // Add document to collection
+            collection.insertOne(doc);
+
+            // Check document in collection
+            Document myDoc = collection.find().first();
+            System.out.println(myDoc.toJson());
+        } catch (Exception e) {
+            System.err.println("An error occurred while connecting to the database or processing the document: " + e.getMessage());
+            e.printStackTrace(); // Print the stack trace for debugging
+        }
     }
 }
+
